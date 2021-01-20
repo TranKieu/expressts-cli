3533994e7f5cf7383999a7d0e09dbceaa60717f5
TranKieu <tranvd2010@gmail.com>
2021-01-20 19:09:36 +0100

    ver 2.0.0

--- src/templates/backend/src/controllers/index.ts
+++ src/templates/backend/src/controllers/index.ts
@@ -1,6 +1,12 @@
 import { Controller } from './controller.interface';
+import { AuthencationController } from './authencation.controller';
+import { UserController } from './user.controller';
 
 /* Test server */
 import { IndexController } from './index.controller';
 
-export const CONTROLLERS: Controller[] = [new IndexController()];
+export const CONTROLLERS: Controller[] = [
+  new UserController(),
+  new AuthencationController(),
+  new IndexController()
+];
